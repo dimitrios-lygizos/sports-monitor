db74596eecb0761e17334d088eabba38c27273eb
dimitrios-lygizos <entomoktono@protonmail.ch>
2024-11-22 23:44:32 +0100

    - postgres initialization via environment variables

---
+++ src/main/java/com/lygizos/sports_monitor/SportsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.lygizos.sports_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsMonitorApplication.class, args);
+	}
+
+}
